fc904795c656d1e453976ebd6719c74bb75ff66b
sorini01 <143932894+sorini01@users.noreply.github.com>
2023-09-17 23:12:33 +0900

    background.js

--- background.js
+++ background.js
@@ -1,1 +1,14 @@
 
+const images = [
+    "purple.jpeg",
+    "blue.jpeg",
+    "green.jpeg",
+];
+
+const ChosenImage = images[Math.floor(Math.random() * images.length)];
+
+const bgImage = document.createElement("img");
+
+bgImage.src = `img/${ChosenImage}`;
+
+document.body.appendChild(bgImage);
